158cb5b23e00eae4d921fbb67b77f42833fd4ca4
Yong <yong.park123456@gmail.com>
2021-04-15 17:10:03 -0600

    Terminado

--- Driver.java
+++ Driver.java
@@ -1,14 +1,45 @@
+//Yong Bum Park 20117
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Driver{
+    public static ArrayList<ArrayList<String>> dic = new ArrayList<ArrayList<String>>();
     public static void main (String[] args){
-        VectorHeap<String> vh = new VectorHeap<>();
-        vh.add("Baba");
-        vh.add("Apple");
-        vh.add("Coco");
-        vh.add("Acid");
-        vh.add("Elephant");
+        //lectura del archivo txt
+        Scanner scanner = new Scanner(System.in);
+            //lectura del diccionario
+            try{
+                File datos = new File("pacientes.txt");
+                Scanner lectura = new Scanner(datos);
 
+                while(lectura.hasNextLine()){
+                    String expresion = lectura.nextLine();
+                    String[] strsplit = expresion.split(",");
+                    ArrayList<String> words = new ArrayList<>();
+                    for(int i=0;i<strsplit.length;i++){
+                        words.add(strsplit[i]);
+                    }
+                    dic.add(words);                
+                }
+                lectura.close();
+            }catch(FileNotFoundException e){
+                System.out.println("No se encontro el archivo");
+                e.printStackTrace();
+            }
+        //ingresar los datos
+        VectorHeap<String> vh = new VectorHeap<>();
+        for(int i=0; i<dic.size();i++){
+            vh.add(dic.get(i).get(2));
+        }
+        //mostrar los pacientes ya ordenados segun la prioridad
         do{
-            System.out.println(vh.getFirst());
+            String prioridad =vh.getFirst();
+            for(int i=0;i<dic.size();i++){
+                if(dic.get(i).contains(prioridad)){
+                    System.out.println(dic.get(i));
+                }
+            }
         }while(vh.isEmpty()==false);
     }
 }

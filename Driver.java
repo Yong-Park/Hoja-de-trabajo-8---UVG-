19102c28b22e6f84f6366c800dcbddf4fa82436b
Yong <yong.park123456@gmail.com>
2021-04-15 07:58:08 -0600

    Priority queue y vectorheap agregado

--- Driver.java
+++ Driver.java
@@ -1,5 +1,12 @@
 public class Driver{
     public static void main (String[] args){
-        
+        VectorHeap<String> vh = new VectorHeap<>();
+        vh.add("B");
+        vh.add("A");
+        vh.add("C");
+        vh.add("E");
+        vh.add("D");
+
+        System.out.println(vh.data);
     }
 }
